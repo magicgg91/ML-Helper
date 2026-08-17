39ffd6851d7f3f1b53ae3826f61267ecb17cf3b9
magicgg91 <16479933+magicgg91@users.noreply.github.com>
2026-08-17 21:55:54 +0200

    Update src/i18n/request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,10 @@
 import { getRequestConfig } from "next-intl/server";
-export default getRequestConfig(async () => ({
-  locale: "en",
-  messages: (await import("../../messages/en.json")).default,
-}));
+import { cookies } from "next/headers";
+export default getRequestConfig(async () => {
+  const locale =
+    (await cookies()).get("NEXT_LOCALE")?.value === "en" ? "en" : "fr";
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
+});
